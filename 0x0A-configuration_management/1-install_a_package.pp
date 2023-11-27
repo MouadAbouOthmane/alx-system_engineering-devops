17d45257ec28cc0d150bfc9fc059b8d662482eaf
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-11-27 22:53:50 +0100

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 # install flask from pip3.
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
 package { 'Flask':
   ensure   => '2.1.0',
+  name     => 'flask',
   provider => 'pip3',
 }
