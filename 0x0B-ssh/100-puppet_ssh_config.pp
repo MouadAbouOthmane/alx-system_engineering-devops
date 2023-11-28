aca6b772eb7f4b75ce020026ae4f926561e19cc5
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-11-28 17:03:04 +0100

    4. Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#  configured to use the private key ~/.ssh/school and refuse to authenticate using a password
+
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
