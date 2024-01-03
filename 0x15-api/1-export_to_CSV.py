c6e92f916650849af363c9016668bf366ccb21ac
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2024-01-03 20:46:39 +0100

    add the username

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,13 +8,20 @@ if __name__ == "__main__":
 
     id = argv[1]
     name_file = '{}.csv'.format(id)
+
+    url_id = f'https://jsonplaceholder.typicode.com/users?id={id}'
     url_todo = f'https://jsonplaceholder.typicode.com/users/{id}/todos'
     todo_list = requests.get(url_todo)
+    user = requests.get(url_id)
 
+    user = user.json()
     todo_list = todo_list.json()
+
+    user_name = user[0]['username']
 
     with open(name_file, 'w') as f:
         for x in todo_list:
-            line = '"{0}","{2}","{1}"\n'.format(x['userId'], x['title'],
-                                                x['completed'])
+            line = '"{0}","{3}","{2}","{1}"\n'.format(
+                x['userId'], x['title'],
+                x['completed'], user_name)
             f.write(line)
