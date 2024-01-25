479b9f7e1c0c40a1549af87962c1bb99b8b369f0
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2024-01-25 09:48:37 +0100

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4 /0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4 /0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # fixes number of open files
 
 exec { 'fix--for-nginx':
-  command => "/bin/sed -i 's/15/3000/' /etc/default/nginx"
+  command => "/bin/sed -i 's/15/3000/g' /etc/default/nginx"
 }
 
 exec { 'restart nginx':
